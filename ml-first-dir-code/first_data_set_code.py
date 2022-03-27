ab4eec19a3cdd8c5f3b46b13bd6b23403e25c313
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-03-27 15:11:34 +0530

    updated

--- ml-first-dir-code/first_data_set_code.py
+++ ml-first-dir-code/first_data_set_code.py
@@ -42,14 +42,13 @@ melbourne_feature = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
 
 ## above columns we are going to give as input
 
-X = melbourne_data[melbourne_feature] ### melbourne data have all data set so in list we going to give few data colunmn
+X = melbourne_data[melbourne_feature]  ### melbourne data have all data set so in list we going to give few data colunmn
 
 print(X.describe())
 
 print(X.head())
 print(X.tail())
 print("------------------------------------------------------------")
-
 
 """ Building YOur Model 
 
@@ -67,10 +66,11 @@ Evaluate ---> Determine how accurate the models prediction are
 """
 
 from sklearn.tree import DecisionTreeRegressor as DTR
+
 # Define model. Specify a number for random_state to ensure same results each run
 melbourne_model = DTR(random_state=1)
 ## Fiting the captured pattern
-melbourne_model.fit(X,y)
+melbourne_model.fit(X, y)
 ### Next step to predict
 print("Making predictions for the following 5 house")
 print(X.head(5))
@@ -90,10 +90,9 @@ home_data2.columns
 
 y2 = home_data2.SalePrice
 
-
 # Creating the list of feature ---> input data going to predict called feature
 
-feature_names = ['LotArea','YearBuilt','1stFlrSF','2ndFlrSF','FullBath','BedroomAbvGr','TotRmsAbvGrd']
+feature_names = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
 
 X2 = home_data2[feature_names]
 
@@ -107,15 +106,13 @@ print(X2.head())
 
 model = DTR(random_state=2)
 
-model.fit(X2,y2)
+model.fit(X2, y2)
 
 predict_value = model.predict(X2)
 
 print(predict_value)
 
-
 print("-------------------------------------------------------------")
-
 
 """ Model Validation  20/03/2022 sunday
 
@@ -143,7 +140,7 @@ filtered_melbourne_data = melbourne_data.dropna(axis=0)
 
 y = filtered_melbourne_data.Price
 melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea',
-                        'YearBuilt', 'Lattitude', 'Longtitude']
+                      'YearBuilt', 'Lattitude', 'Longtitude']
 X = filtered_melbourne_data[melbourne_features]
 
 from sklearn.tree import DecisionTreeRegressor
@@ -155,10 +152,10 @@ melbourne_model.fit(X, y)
 
 ## we have import the mean absolute error
 
-from  sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_error
 
 predicted_home_prices = melbourne_model.predict(X)
-mean_absolute_error(y,predicted_home_prices)
+mean_absolute_error(y, predicted_home_prices)
 
 """ The Problem with above Data accurate 
 we were used the trained data in our prediction so the data accurate is good
@@ -181,7 +178,7 @@ from sklearn.model_selection import train_test_split
 # The split is based on a random number generator. Supplying a numeric value to
 # the random_state argument guarantees we get the same split every time we
 # run this script.
-train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
 # Define model
 melbourne_model = DecisionTreeRegressor()
 # Fit model
@@ -190,7 +187,6 @@ melbourne_model.fit(train_X, train_y)
 # get predicted prices on validation data
 val_predictions = melbourne_model.predict(val_X)
 print(mean_absolute_error(val_y, val_predictions))
-
 
 """" Prediction 3 -- Model validation """
 
@@ -206,7 +202,7 @@ y3 = home_data3.SalePrice
 
 ## features -- input
 
-feature_columns=['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+feature_columns = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
 
 home_data3.describe()
 
@@ -214,24 +210,26 @@ X3 = home_data3[feature_columns]
 
 ## define the model
 
-home_model3= DTR(random_state=1)
+home_model3 = DTR(random_state=1)
 
 # fitting the model
 
-home_model3.fit(X3,y3)
+home_model3.fit(X3, y3)
 
 print("First in-sample predictions:", home_model3.predict(X3.head()))
 print("Actual target values for those homes:", y3.tail().tolist())
 
 ## using the splitting data
 from sklearn.model_selection import train_test_split as tts
-train_X3, val_X3, train_y3, val_y3 = tts(X3,y3,random_state=1)
+
+train_X3, val_X3, train_y3, val_y3 = tts(X3, y3, random_state=1)
 
 ## again fitting the model
 from sklearn.tree import DecisionTreeRegressor as DTR3
+
 home_test_model3 = DTR3(random_state=1)
 
-home_test_model3.fit(train_X3,train_y3)
+home_test_model3.fit(train_X3, train_y3)
 
 ## Make predictions with validation of data
 
@@ -242,37 +240,74 @@ print(val_predictions_3)
 # print the top few validation predictions
 print(home_test_model3.predict(X3.head()))
 # print the top few actual prices from validation data
-print( y3.tail().tolist())
+print(y3.tail().tolist())
 
 ## final to calculate the mean absolute error in validation data
-from  sklearn.metrics import mean_absolute_error as mbe
-val_mae = mbe(val_predictions_3,val_y3)
+from sklearn.metrics import mean_absolute_error as mbe
+
+val_mae = mbe(val_predictions_3, val_y3)
 
 print(val_mae)
 
+print("-----------------------------------------------------------------------")
 
-""" Underfitting and Overfitting Sunday 27/03/2020
+"""  Underfitting and Overfitting Sunday 27/03/2020
  
  Overfitting --->  This is a phenomenon called overfitting, where a model matches the training data almost perfectly, 
                   but does poorly in validation and other new data. On the flip side, if we make our tree very shallow, 
                   it doesn't divide up the houses into very distinct groups.
 
 
- Underfitting --->  
+ Underfitting --->  At an extreme, if a tree divides houses into only 2 or 4, each group still has a wide variety of houses. 
+                   Resulting predictions may be far off for most houses, 
+                   even in the training data (and it will be bad in validation too for the same reason). 
+                   When a model fails to capture important distinctions and patterns in the data, 
+                   so it performs poorly even in training data, that is called underfitting.
  
+Overfitting: capturing spurious patterns that won't recur in the future, leading to less accurate predictions, or
+Underfitting: failing to capture relevant patterns, again leading to less accurate predictions.
+
+ SOLUTION TO CONTROL THE OVER AND UNDER FITTING
+ 
+The max_leaf_nodes argument provides a very sensible way to control overfitting vs underfitting. t
+the more leaves we allow the model to make, 
+the more we move from the underfitting area in the above graph to the overfitting area.
  
  
  """
 
+### Prediction 4
+
+melbourne_file_path4 = "/Volumes/Macintosh HD/For Mac/python project/Machine_Learning/csv-data set/melb_data.csv"
+
+melbourne_data4 = pd.read_csv(melbourne_file_path4)
+
+# Filter rows with missing values
+filtered_melbourne_data4 = melbourne_data4.dropna(axis=0)
+
+# Choose target and features
+y4 = filtered_melbourne_data4.Price
+
+melbourne_features4 = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea',
+                       'YearBuilt', 'Lattitude', 'Longtitude']
+
+X4 = filtered_melbourne_data4[melbourne_features4]
+
+from sklearn.model_selection import train_test_split
+
+# split data into training and validation data, for both features and target
+train_X4, val_X4, train_y4, val_y4 = train_test_split(X4, y4, random_state=0)
 
 
+def get_mae(max_leaf_nodes4, train_X4, val_X4, train_y4, val_y4):
+    model4 = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes4, random_state=0)
+    model4.fit(train_X4, train_y4)
+    preds_val4 = model4.predict(val_X4)
+    mae4 = mean_absolute_error(val_y4, preds_val4)
+    return (mae4)
 
 
-
-
-
-
-
-
-
-
+# compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes4 in [5,50,500]:
+    my_mae4 = get_mae(max_leaf_nodes4, train_X4, val_X4, train_y4, val_y4)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" % (max_leaf_nodes4, my_mae4))
