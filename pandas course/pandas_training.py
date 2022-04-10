7d7bdbf78ab90ae53401182e96659af13addbd42
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-10 10:48:36 +0530

    pandas

---
+++ pandas course/pandas_training.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+"""
+What is pandas ---> its one of the python library for data analysis.
+"""
+
+## Creating data using DataFrame
+
+""" A DataFrame is a table, its contains an array of individual entries """
+
+print(pd.DataFrame(
+    {
+    'Yes':[50,32],
+     'No':[232,3]
+    }
+   )
+)
+
+print(pd.DataFrame(
+    {
+     'Title':['My name is walker'],
+      'Learn':['Pandas'],
+    }
+  )
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
